70f3d4257d8256a89553cc6e69627602da6e3796
Anna Romashenko <aa.romashchenko@gmail.com>
2020-05-12 16:33:44 +0300

    problem with map

--- src/pages/movieDetailsPage/MovieDetailsPage.js
+++ src/pages/movieDetailsPage/MovieDetailsPage.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Cast } from './cast/Cast';
 import { Reviews } from './reviews/Reviews';
-import { NavLink, Switch, Route, Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 // import { withRouter } from 'react-router-dom';
 import moviesAPI from '../../services/movies-api';
 
 // import css from './MovieDetailsPage.module.css';
+
+// let ganresItems;
+// if (movie) {
+// 	ganresItems = movie.map((ganre) => {
+// 		return <li key={ganre.id}>{ganre.name}</li>;
+// 	});
+// 	return <li>{ganresItems}</li>;
+// }
 
 class MovieDetailsPage extends Component {
   state = {
@@ -16,57 +24,56 @@ class MovieDetailsPage extends Component {
     const id = this.getMovieId(this.props);
     moviesAPI
       .getMovieDetails(id)
-      .then(movie => this.setState({ movie: movie.data }));
+      .then(movie => this.setState({ movie: movie.data, id }));
+    console.log('componentDidMount');
+    // console.log(this.state.movie.genres.length);
   }
 
   getMovieId = props => props.match.params.movieId;
 
   render() {
-    console.log(this.props.match);
+    console.log(this.state);
+    console.log('match: ', this.props.match);
     const { movie } = this.state;
-    console.log(movie.genres);
-
-    // const ganreToSee = movie.genres.map(ganre => {
-    //   return ganre.name;
-    // });
-    // console.log(ganreToSee);
+    console.log('movie: ', movie);
+    const ganres = movie.genres;
+    console.log(ganres);
 
     return (
       <div>
-        {movie !== 0 && (
-          <>
+        {movie && (
+          <Fragment>
             <button>&#8592; Go back</button>
             <br />
             <img
               src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
               alt=""
             />
-            /
+
             <section>
               <h2>{movie.title}</h2>
               <span>(Original: {movie.original_title})</span>
               <p>User score: {Math.round(movie.popularity)}%</p>
               <h3>Overview</h3>
               <p>{movie.overview} </p>
-              <h4>Genres</h4>
-              {/* <ul>
-                {movie.genres !== 0 && (
-                  <>
-                    {movie.genres.map(genre => (
-                      <li key={genre.id}>{genre.name}</li>
-                    ))}
-                  </>
-                )}
-              </ul> */}
+              <h4>Genres: </h4>
+              <ul>
+                {/* <p>{ganres[0]}</p> */}
+                {/* {movie.ganres !== 0 && (
+									<Fragment>
+										{movie.ganres.map((genre) => <li key={genre.id}>{genre.name}</li>)}
+									</Fragment>
+								)} */}
+              </ul>
             </section>
             <section>
               <p>Additional information</p>
               <ul>
                 <li>
-                  <Link>Cast</Link>
+                  <Link to="/movies/:movieId/cast">Cast</Link>
                 </li>
                 <li>
-                  <Link>Reviews</Link>
+                  <Link to="/movies/:movieId/reviews">Reviews</Link>
                 </li>
               </ul>
             </section>
@@ -74,7 +81,7 @@ class MovieDetailsPage extends Component {
             <Route path="/movies/:movieId/cast" component={Cast} />
             <Route path="/movies/:movieId/reviews" component={Reviews} />
             {/* </Switch> */}
-          </>
+          </Fragment>
         )}
       </div>
     );
