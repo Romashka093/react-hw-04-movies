84055c819b8210dbb66f5d4a64941134defb7422
Anna Romashenko <aa.romashchenko@gmail.com>
2020-05-13 23:19:25 +0300

    Updates

---
+++ precache-manifest.121690718402d9d54710257aa0594912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d88483d503ddcdcd35f0e47a08a1c61",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "f9b06fd9169aaf02f13e",
+    "url": "/react-hw-04-movies/static/css/main.50671028.chunk.css"
+  },
+  {
+    "revision": "1cbfcb462b7c8b453afd",
+    "url": "/react-hw-04-movies/static/js/2.365a57bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-04-movies/static/js/2.365a57bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b06fd9169aaf02f13e",
+    "url": "/react-hw-04-movies/static/js/main.b0fa306e.chunk.js"
+  },
+  {
+    "revision": "6d1bfb49ca51b291adba",
+    "url": "/react-hw-04-movies/static/js/runtime-main.314de086.js"
+  }
+]);
